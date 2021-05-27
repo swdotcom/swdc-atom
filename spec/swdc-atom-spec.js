5ed1d0986c37ae741727aef5907876c5907ea881
Xavier Luiz <xavluiz@gmail.com>
2021-05-27 14:18:24 -0700

    add current day stats ws listener and cleanup

--- spec/swdc-atom-spec.js
+++ spec/swdc-atom-spec.js
@@ -3,7 +3,6 @@
 import SwdcAtom from '../lib/swdc-atom';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
